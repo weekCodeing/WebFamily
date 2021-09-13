cae9a545016d56f359a91e86c39625eaa3d58a4f
webVueBlog <2979986590@qq.com>
2021-09-14 00:00:19 +0800

    element-test

--- element-test/babel.config.js
+++ element-test/babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  "presets": [["es2015", { "modules": false }]],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
+  // '@vue/cli-plugin-babel/preset'
 }
--- element-test/src/views/Home.vue
+++ element-test/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <el-button @click="show">点我</el-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    show() {
+      this.$message.success('Toast from element-ui')
+    }
   }
 }
 </script>
+
